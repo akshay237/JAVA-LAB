82a42e13c4dd14820babf6f6515be3a11bca5bb8
shashwat varshney <44412994+shashwatvarshney@users.noreply.github.com>
2019-05-27 01:15:55 +0530

    Add files via upload

---
+++ pg1/src/pg1/MissedCall.java
@@ -0,0 +1,21 @@
+package pg1;
+
+import java.util.*;
+public class MissedCall {
+	
+	Scanner sc = new Scanner(System.in);
+	Calendar cd;
+	Long num;
+	String name;
+	MissedCall(Calendar c,Long nu,String n)
+	{
+		cd=c;
+		num=nu;
+		name=n;
+	}
+	void display()
+	{
+		System.out.println("Name :"+name+"\n Number :"+num+"\n Time :"+cd.getTime());
+	}
+	
+}
